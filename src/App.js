56f168c7be0b749aeedd5e343499066fa668360d
Simon <Monkeymandeveloper@protonmail.com>
2022-08-28 14:40:54 +0200

    misstype

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           setSearchTerm(event.target.value);
         }}
       />
-      <h2>Ex: Monkey, Salmon, Racoon...</h2>
+      <h2>Ex: Monkey, Salmon, Raccoon...</h2>
       {JSONDATA.filter((val)=> {
         if(searchTerm === '') {
           return val
